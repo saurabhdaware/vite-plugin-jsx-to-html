e28c7ea95bcc892280a81952d21e4b7e39fe886d
saurabhdaware <saurabhdaware99@gmail.com>
2023-05-10 18:28:09 +0530

    fix: dependencies

--- vite.config.ts
+++ vite.config.ts
@@ -3,7 +3,10 @@ import mdx from "@mdx-js/rollup";
 import { vitePluginJSXToHTML } from './plugin/index';
 
 export default defineConfig({
-  plugins: [mdx(), vitePluginJSXToHTML({
-    extensions: ['.mdx']
-  })],
+  plugins: [
+    mdx(), // This turns MDX into React Components
+    vitePluginJSXToHTML({
+      extensions: ['.mdx'] // This turns React Components into HTML string
+    })
+  ],
 })
